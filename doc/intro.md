069b3d40748eb0ae7c48a88cb1f3f34f5101b7c8
Geoff Shannon <geoffpshannon@gmail.com>
2017-01-22 16:55:20 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kata-viewer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kata_viewer/core_test.clj
@@ -0,0 +1,7 @@
+(ns kata-viewer.core-test
+  (:require [clojure.test :refer :all]
+            [kata-viewer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
