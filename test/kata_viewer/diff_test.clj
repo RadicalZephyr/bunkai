910d678692b4747ba9d1fb11153a02dd706237fb
Geoff Shannon <geoffpshannon@gmail.com>
2017-01-23 01:15:43 -0600

    Add redundant remove non-first line test and group tests

--- test/kata_viewer/diff_test.clj
+++ test/kata_viewer/diff_test.clj
@@ -5,13 +5,18 @@
 ;; Line-based diffing and patching tools
 
 (t/deftest test-diff
-  (t/is (= {0  [:+ "Hi!"]}
-           (sut/diff "foo.clj" [] ["Hi!"])))
+  (t/testing "Basic line addition"
+    (t/is (= {0  [:+ "Hi!"]}
+             (sut/diff "foo.clj" [] ["Hi!"])))
 
-  (t/is (= {1  [:+ "Bob"]}
-           (sut/diff "foo.clj"
-                     ["Hi!"]
-                     ["Hi!" "Bob"])))
+    (t/is (= {1  [:+ "Bob"]}
+             (sut/diff "foo.clj"
+                       ["Hi!"]
+                       ["Hi!" "Bob"]))))
 
-  (t/is (= {0  [:- "Hi!"]}
-           (sut/diff "foo.clj" ["Hi!"] []))))
+  (t/testing "Basic line removal"
+    (t/is (= {0  [:- "Hi!"]}
+             (sut/diff "foo.clj" ["Hi!"] [])))
+
+    (t/is (= {1  [:- "Bob"]}
+             (sut/diff "foo.clj" ["Hi!" "Bob"] ["Hi!"])))))
